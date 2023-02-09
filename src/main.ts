632a73134bbf690bb5e37cbbca471ec43600ddc0
Victoria Cristina Belo <victoria.belo@CONTABILIZEI.COM.BR>
2023-02-09 12:02:36 -0300

    config. cors

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true  });
   app.enableCors({
-    origin: ['*', 'https://victoria-belo.github.io/file'],      
+    origin: ['*', 'https://victoria-belo.github.io/file', 'https://victoria-belo.github.io/Buffer-Generator/#/upload/AppController_Hi','https://victoria-belo.github.io/Buffer-Generator/#/file/AppController_Hi' ],      
     methods: ["GET", "POST"],
   });
   const config = new DocumentBuilder()
     .setTitle('GeneratorCSV Buffer')
     .setDescription('A simple file uploading. CSV only.')
-    .setVersion('1.0')
-    .addServer('https://victoria-belo.github.io/file')
+    .setVersion('1.0')    
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document,
