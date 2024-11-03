c7b85a49c4121c2da524d7c718ea54dc5d5d18cd
unknown <vickbelo17@gmail.com>
2024-11-03 00:12:47 -0300

    fix confs

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,10 @@ async function bootstrap() {
     .setDescription('A simple file uploading. CSV only.')
     .setVersion('1.0')    
     .build();
+    
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document,
-  {swaggerOptions: {defaultModelsExpandDepth:-1}});
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
